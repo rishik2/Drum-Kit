14fc50f97f4bf8458c806a4a847d6f777db6f279
Rishi Kohli <rishikohli@Rishis-MacBook-Air.local>
2021-11-25 11:40:14 +0530

    My Drum Kit

---
+++ index.js
@@ -0,0 +1,70 @@
+var buttons = document.querySelectorAll('.drum')
+
+// for mouse clicked
+
+
+
+for (var i = 0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', function(){
+        var key = this.innerHTML
+        makeSound(key)
+        makeAnimation(key)
+    })
+    
+}
+
+
+// for keyboard event
+
+
+
+document.addEventListener("keypress", function(event){
+    //makeSound(event.Key)
+    makeSound(event.key)
+    makeAnimation(event.key)
+})
+
+
+
+
+var makeSound = function(key){
+    switch (key) {
+          case "w":
+              var sound = new Audio("sounds/crash.mp3")
+              sound.play()
+              break;
+          case "a":
+              var sound = new Audio("sounds/kick-bass.mp3")
+              sound.play()
+              break;
+          case "s":
+              var sound = new Audio("sounds/snare.mp3")
+              sound.play()
+              break;
+          case "d":
+              var sound = new Audio("sounds/tom-1.mp3")
+              sound.play()
+              break;
+          case "j":
+              var sound = new Audio("sounds/tom-2.mp3")
+              sound.play()
+              break;
+          case "k":
+              var sound = new Audio("sounds/tom-3.mp3")
+              sound.play()
+              break;
+          case "l":
+              var sound = new Audio("sounds/tom-4.mp3")
+              sound.play()
+              break;
+          default:
+              console.log("Button not found")
+      }
+}
+
+var makeAnimation = function(key){
+    document.querySelector("." + key).classList.add("pressed")
+    setTimeout(function(){
+        document.querySelector("." + key).classList.remove("pressed")
+    },100)
+}
